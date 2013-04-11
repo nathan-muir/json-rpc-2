bff75acc591bf47584a15f07a03ea18af467c5f4
Nathan Muir <ndmuir@gmail.com>
2013-04-11 10:54:59 +1000

    added json pretty printing code.

--- src/Ndm/JsonRpc2/Core/Json.php
+++ src/Ndm/JsonRpc2/Core/Json.php
@@ -10,7 +10,9 @@ class Json
     /**
      * @param string $json
      * @param int $depth
+     *
      * @return mixed
+     *
      * @throws Exception\JsonParseException
      */
     public static function decode($json, $depth)
@@ -52,10 +54,91 @@ class Json
 
     /**
      * @param mixed $data
+     * @param bool $pretty
+     *
      * @return string
      */
-    public static function encode($data)
+    public static function encode($data, $pretty=false)
     {
-        return json_encode($data);
+        if (!$pretty){
+            return json_encode($data);
+        } elseif(version_compare(phpversion(), '5.4', '>=')) {
+            return json_encode($data, JSON_PRETTY_PRINT);
+        } else {
+            return self::pretty_fallback(json_encode($data));
+        }
+    }
+
+    /**
+     * @param string $json
+     *
+     * @return string
+     */
+    public static function pretty($json){
+        if(version_compare(phpversion(), '5.4', '>=')){
+            return self::encode(self::decode($json,512),true);
+        } else {
+            return self::pretty_fallback($json);
+        }
+    }
+
+    /**
+     * From http://stackoverflow.com/a/9776726/494182
+     *
+     * @param string $json
+     *
+     * @return string
+     */
+    private static function pretty_fallback($json){
+        $result = '';
+        $level = 0;
+        $prev_char = '';
+        $in_quotes = false;
+        $ends_line_level = null;
+        $json_length = strlen( $json );
+
+        for( $i = 0; $i < $json_length; $i++ ) {
+            $char = $json[$i];
+            $new_line_level = null;
+            $post = "";
+            if( $ends_line_level !== null ) {
+                $new_line_level = $ends_line_level;
+                $ends_line_level = null;
+            }
+            if( $char === '"' && $prev_char != '\\' ) {
+                $in_quotes = !$in_quotes;
+            } else if( ! $in_quotes ) {
+                switch( $char ) {
+                    case '}': case ']':
+                        $level--;
+                        $ends_line_level = null;
+                        $new_line_level = $level;
+                        break;
+
+                    case '{': case '[':
+                        $level++;
+                    case ',':
+                        $ends_line_level = $level;
+                        break;
+
+                    case ':':
+                        $post = " ";
+                        break;
+
+                    case " ": case "\t": case "\n": case "\r":
+                        $char = "";
+                        $ends_line_level = $new_line_level;
+                        $new_line_level = null;
+                        break;
+                }
+            }
+            if( $new_line_level !== null ) {
+                $result .= "\n".str_repeat( "\t", $new_line_level );
+            }
+            $result .= $char.$post;
+            $prev_char = $char;
+        }
+
+        return $result;
     }
 }
